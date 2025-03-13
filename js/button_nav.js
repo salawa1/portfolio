12654556fddfba7959af04c55de622606f20bff6
samalawadhi <52089312+samalawadhi@users.noreply.github.com>
2025-03-13 11:18:03 -0400

    First commit, just saving work so far and testing

---
+++ js/button_nav.js
@@ -0,0 +1,11 @@
+document.getElementById("btnHome").addEventListener("click", function() {
+    window.location.href = "home.html";
+});
+
+document.getElementById("btnAbout").addEventListener("click", function() {
+    window.location.href = "about.html";
+});
+
+document.getElementById("btnContact").addEventListener("click", function() {
+    window.location.href = "contact.html";
+});
