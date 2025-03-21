8ce271d0ada2f956ee7a9ee404f686dd70c74564
samalawadhi <52089312+samalawadhi@users.noreply.github.com>
2025-03-20 20:00:58 -0400

    Completed first iteration of portfolio

--- js/button_nav.js
+++ js/button_nav.js
@@ -2,8 +2,8 @@ document.getElementById("btnHome").addEventListener("click", function() {
     window.location.href = "home.html";
 });
 
-document.getElementById("btnAbout").addEventListener("click", function() {
-    window.location.href = "about.html";
+document.getElementById("btnProjects").addEventListener("click", function() {
+    window.location.href = "projects.html";
 });
 
 document.getElementById("btnContact").addEventListener("click", function() {
